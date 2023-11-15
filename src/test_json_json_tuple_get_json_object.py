a8dcc247091c7c768b8c0106387d55ef5a2e1e93
Tariqul Islam <tariqul.islam.rony@gmail.com>
2023-11-15 15:23:50 +0900

    fixing the write json file into directory and read file functions and data and file

--- src/test_json_json_tuple_get_json_object.py
+++ src/test_json_json_tuple_get_json_object.py
@@ -2,36 +2,52 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
+APP_DIR="/home/hadoop/spark-json-example"
+
 class TestGetJsonTextExtract:
 
-     # extract the json String
+     # test the get json object and json tuple function
      def test_get_json_object_tuple_func(self):
+        # Create the Spark Session to handle the spark Operationn
+        # Using spark "spark://localhost:7707" as master to handle 
+        # Resource and operation
         spark = SparkSession.builder\
              .master("spark://localhost:7077")\
-             .appName("CreateJsonFormatField")\
+             .appName("TestGetJsonTextExtract")\
              .getOrCreate()
         
+        # Read the csv file from data folder
+        # add the schema option inferSchema True, It will create schema
+        # from data source
         df=spark.read.format('csv')\
             .option("inferSchema", True)\
             .option('header', True)\
-            .load(\
-         "file:///home/hadoop/spark-json-example/data/flim.csv")
+            .load(f"file://{APP_DIR}/data/flim.csv")
         df.show()
         df.printSchema()
 
       
-        
+        # Using get_json_object 
+        # SparkSQL function (column_name, "$.json_attribute_name")
+        # Extract the different json attribute and value 
+        # to new dataframe columns and value
         df2 = df.select(
-                F.col("id"),
-                F.get_json_object(F.col("movie"), "$.title").alias("title"),
-                F.get_json_object(F.col("movie"), "$.rating").alias("rating")
+             F.col("id"),
+             F.get_json_object(F.col("movie"), "$.Title").alias("Title"),
+             F.get_json_object(F.col("movie"), "$.Rated").alias("Rated"),
+             F.get_json_object(F.col("movie"), "$.Year").alias("Year"),
+             F.get_json_object(F.col("movie"), "$.Released").alias("Released")
         )
         df2.show()
         df2.printSchema()
 
-        # Json Tuple function
+        # Using Json Tuple function 
+        # json_tuple(column_name, "json_attr_1", "json_attr_2", ....)
+        # Extract the each attribute into new dataframe column
         df3 = df.select(F.col("id"),
-                         F.json_tuple(F.col("movie"), "title", "rating"))
+                        F.json_tuple(F.col("movie"),
+                        "Title", "Rated", "Released", "Year")
+                        .alias("Title", "Rated", "Released", "Year"))
         df3.show()
         df3.printSchema()
 
--- src/test_json_write_files.py
+++ src/test_json_write_files.py
@@ -2,47 +2,67 @@ from pyspark.sql import SparkSession, \
     functions as F, \
         types as T
 
+APP_DIR="/home/hadoop/spark-json-example"
 
 class WriteJsonFile:
     def test_write_json_file(self):
+        # Create the Spark Session to handle the spark Operationn
+        # Using spark "spark://localhost:7707" as master to handle 
+        # Resource and operation
         spark = SparkSession.builder\
             .master("spark://localhost:7077")\
             .appName("CreateJsonFormatField")\
             .getOrCreate()
         
+        # Read the csv file from data folder
+        # add the schema option inferSchema True, It will create schema
+        # from data source
         df=spark.read.format('csv')\
             .option("inferSchema", True)\
             .option('header', True)\
-            .load(\
-         "file:///home/hadoop/spark-json-example/data/flim.csv")
+            .load(f"file://{APP_DIR}/data/flim.csv")
         df.show()
         df.printSchema()
-         
+
+        # create the schema to read the json text field from flim
+        # dataframe using struct type and struct field 
         schema = T.StructType(
             [
-                T.StructField('title', T.StringType(), True),
-                T.StructField('rating', T.StringType(), True),
-                T.StructField('releaseYear', T.StringType(), True),
-                T.StructField('genre', T.StringType(), True)
+                T.StructField('Title', T.StringType(), True),
+                T.StructField('Year', T.StringType(), True),
+                T.StructField('Rated', T.StringType(), True),
+                T.StructField('Released', T.StringType(), True),
+                T.StructField('Genre', T.StringType(), True)
             ]
         )
-
+        
+        # Using the withColumn function 
+        # which transforms string json field (movie)
+        # to data structure which is defined into schema variable
         movie_df_mapped = df.withColumn("movie", F.from_json("movie", schema))
         movie_df_mapped.show(truncate=False)
         movie_df_mapped.printSchema()
 
+        # Create the New Data frame to generate the Json file by 
+        # Select function
         movie_df = movie_df_mapped.select(F.col('id'), \
-                                         F.col("movie.title"),\
-                                          F.col("movie.rating"),\
-                                            F.col("movie.releaseYear"), \
-                                              F.col("movie.genre"))
+                                         F.col("movie.Title"),\
+                                         F.col("movie.Year"),\
+                                         F.col("movie.Rated"),\
+                                         F.col("movie.Released"),\
+                                         F.col("movie.Genre"))
         movie_df.show(truncate=False)
         movie_df.printSchema()
-
+        # using the spark Data frame write function
+        # with mode is overwrite (which will replace the data everytime
+        # write the datasource into specific location
+        # Json write will by default take english format datetime
+        # we have to specifiy the specify the specific datetime 
+        # format is the data frame has datetime field
         movie_df.write\
         .mode("overwrite")\
         .option("dateFormat", "MM/dd/yyyy")\
-       .json("file:///home/hadoop/spark-json-example/data/modified-movie")
+       .json(f"file://{APP_DIR}/data/modified-movie")
 
 if __name__ == '__main__':
   write_json_file = WriteJsonFile()
